4c7b706fe219b24c17dc25aa79515b9b305ba6ad
janiterea <janiterea@gmail.com>
2026-05-29 19:01:57 +0800

    final changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,19 +1,17 @@
 const currentYear = new Date().getFullYear();
 document.getElementById("currentYear").textContent = currentYear;
-document.getElementById("lastModified").innerHTML = document.lastModified;
-
-document.body.style.backgroundColor = ""
+document.getElementById("lastModified").textContent = document.lastModified;
 
 const temperature = 10;
-const windSpeed = 5;
+const windSpeed = 5.2;
 function calculateWindChill(temp, wind) {
-    return 13.12 + (0.6215 * temperature) - (11.37 * windSpeed ** 0.16) + (0.3965 * temperature * windSpeed ** 0.16);
+    return 13.12 + (0.6215 * temp) - (11.37 * wind ** 0.16) + (0.3965 * temp * wind ** 0.16);
 }
 
 let windChill;
 
 if (temperature <= 10 && windSpeed > 4.8) {
-    windChill = `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+    windChill = `${calculateWindChill(temperature, windSpeed).toFixed(1)}°C`;
 } else {
     windChill = "N/A";
 }
