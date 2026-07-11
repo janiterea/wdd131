ffdbfc1ca162e273d218fc6c7a3194b6631bb656
janiterea <janiterea@gmail.com>
2026-07-11 16:28:40 +0800

    Handling DOM Events Activity

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document. querySelector('#list');
+
+// wait for button clicks
+button.addEventListener('click', function() {
+    // Check if the user entered something
+    if (input.value != '') {
+        // create list item and give it the value of the input
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        // create a button and add a click event listener
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        // add the button to the list item
+        li.appendChild(deleteButton);
+        // OUTPUT: finally display the completed list item to the unordered list
+        list.appendChild(li);
+        input.value = '';
+    }
+    // focus the user back to the input field
+    input.focus();
+});
+
+
